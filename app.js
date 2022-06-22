b79a954a105810954a84b43fefc3f56258dbb560
bbaltuntas <borabaris.01@gmail.com>
2022-06-22 20:57:02 +0300

    Second level of encryption-Hashing

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express")
 const mongoose = require("mongoose")
 const ejs = require("ejs")
 const encrypt = require("mongoose-encryption")
+const md5 = require("md5")
 
 const app = express()
 
@@ -23,10 +24,6 @@ const userSchema = new mongoose.Schema({
     password: String,
 })
 
-var secret = process.env.SOME_LONG_UNGUESSABLE_STRING;
-
-userSchema.plugin(encrypt, { secret:secret, encryptedFields: ['password'] });
-
 
 const User = mongoose.model("User", userSchema)
 
@@ -45,7 +42,7 @@ app.route("/register")
     .post((req, res) => {
         const newUser = new User({
             email: req.body.email,
-            password: req.body.password,
+            password: md5(req.body.password),
         })
         newUser.save(function (err) {
             if (!err) {
@@ -63,7 +60,7 @@ app.route("/login")
     })
     .post((req, res) => {
         const email = req.body.email
-        const password = req.body.password
+        const password = md5(req.body.password)
         User.findOne({email: email}, function (err, foundUser) {
             if (err) {
                 console.log(err)
@@ -73,7 +70,7 @@ app.route("/login")
                     if (foundUser.password === password) {
                         res.render("secrets")
                     }
-                }else{
+                } else {
                     console.log("kullanıcı yok")
                 }
             }
